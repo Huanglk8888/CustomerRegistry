5bc113cb77533537e808f76409f123a5b9ba9e2b
黑柳 <463927289@qq.com>
2020-11-03 22:37:19 +0800

    基于AtomicStampedReference解决注册表缓存ABA问题

--- register-client/src/main/java/com/hlk/demo/register/client/CachedServiceRegistry.java
+++ register-client/src/main/java/com/hlk/demo/register/client/CachedServiceRegistry.java
@@ -3,7 +3,7 @@ package com.hlk.demo.register.client;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * 客户端缓存注册表
@@ -18,8 +18,7 @@ public class CachedServiceRegistry {
     /**
      * 客户端缓存的所有的服务实例的信息
      */
-    private AtomicReference<Applications> applications =
-            new AtomicReference<Applications>(new Applications());
+    private AtomicStampedReference<Applications> applications;
     /**
      * 客户端缓存的服务注册表
      */
@@ -45,6 +44,7 @@ public class CachedServiceRegistry {
         this.fetchDeltaRegistryWorker = new FetchDeltaRegistryWorker();
         this.registerClient = registerClient;
         this.httpSender = httpSender;
+        this.applications = new AtomicStampedReference<Applications>(new Applications(), 0);
     }
 
 
@@ -73,9 +73,8 @@ public class CachedServiceRegistry {
      * @return
      */
     public Map<String, Map<String, ServiceInstance>> getRegistry() {
-        return applications.get().getRegistry();
+        return applications.getReference().getRegistry();
     }
-
 
 
     /**
@@ -85,7 +84,7 @@ public class CachedServiceRegistry {
      */
     private void reconcileRegistry(DeltaRegistry deltaRegistry) {
         Map<String, Map<String, ServiceInstance>> registry =
-                applications.get().getRegistry();
+                applications.getReference().getRegistry();
 
         Long serverSideTotalCount = deltaRegistry.getServiceInstanceTotalCount();
 
@@ -98,64 +97,11 @@ public class CachedServiceRegistry {
             // 重新拉取全量注册表进行纠正
             Applications fetchedApplications = httpSender.fetchFullRegistry();
             while (true) {
-                Applications expectedApplications = applications.get();
-                if (applications.compareAndSet(expectedApplications, fetchedApplications)) {
+                Applications expectedApplications = applications.getReference();
+                int expectedStamp = applications.getStamp();
+                if(applications.compareAndSet(expectedApplications, fetchedApplications,
+                        expectedStamp, expectedStamp + 1)) {
                     break;
-                }
-            }
-        }
-    }
-
-
-
-    /**
-     * 全量拉取注册表的后台线程
-     *
-     * @author huanglk
-     */
-    private class FetchFullRegistryWorker extends Thread {
-        @Override
-        public void run() {
-            // 拉取全量注册表
-            Applications fetchedApplications = httpSender.fetchFullRegistry();
-            while (true) {
-                Applications expectedApplications = applications.get();
-                if (applications.compareAndSet(expectedApplications, fetchedApplications)) {
-                    break;
-                }
-            }
-        }
-    }
-
-    /**
-     * 增量拉取注册表的后台线程
-     *
-     * @author huanglk
-     */
-    private class FetchDeltaRegistryWorker extends Thread {
-        @Override
-        public void run() {
-            while (registerClient.isRunning()) {
-                try {
-                    Thread.sleep(SERVICE_REGISTRY_FETCH_INTERVAL);
-
-                    // 拉取回来的是最近3分钟变化的服务实例
-                    DeltaRegistry deltaRegistry = httpSender.fetchDeltaRegistry();
-
-                    // 一类是注册，一类是删除
-                    // 如果是注册的话，就判断一下这个服务实例是否在这个本地缓存的注册表中
-                    // 如果不在的话，就放到本地缓存注册表里去
-                    // 如果是删除的话，就看一下，如果服务实例存在，就给删除了
-
-                    // 我们这里其实是要大量的修改本地缓存的注册表，所以此处需要加锁
-                    mergeDeltaRegistry(deltaRegistry);
-                    // 再检查一下，跟服务端的注册表的服务实例的数量相比，是否是一致的
-                    // 封装一下增量注册表的对象，也就是拉取增量注册表的时候，一方面是返回那个数据
-                    // 另外一方面，是要那个对应的register-server端的服务实例的数量
-                    reconcileRegistry(deltaRegistry);
-
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
             }
         }
@@ -169,7 +115,7 @@ public class CachedServiceRegistry {
     private void mergeDeltaRegistry(DeltaRegistry deltaRegistry) {
         synchronized (applications) {
             Map<String, Map<String, ServiceInstance>> registry =
-                    applications.get().getRegistry();
+                    applications.getReference().getRegistry();
 
             LinkedList<RecentlyChangedServiceInstance> recentlyChangedQueue =
                     deltaRegistry.getRecentlyChangedQueue();
@@ -178,26 +124,26 @@ public class CachedServiceRegistry {
                 String serviceName = recentlyChangedItem.serviceInstance.getServiceName();
                 String serviceInstanceId = recentlyChangedItem.serviceInstance.getServiceInstanceId();
                 // 如果是注册操作的话
-                if(ServiceInstanceOperation.REGISTER.equals(
+                if (ServiceInstanceOperation.REGISTER.equals(
                         recentlyChangedItem.serviceInstanceOperation)) {
                     Map<String, ServiceInstance> serviceInstanceMap = registry.get(serviceName);
-                    if(serviceInstanceMap == null) {
+                    if (serviceInstanceMap == null) {
                         serviceInstanceMap = new HashMap<String, ServiceInstance>();
                         registry.put(serviceName, serviceInstanceMap);
                     }
 
                     ServiceInstance serviceInstance = serviceInstanceMap.get(serviceInstanceId);
-                    if(serviceInstance == null) {
+                    if (serviceInstance == null) {
                         serviceInstanceMap.put(serviceInstanceId,
                                 recentlyChangedItem.serviceInstance);
                     }
                 }
 
                 // 如果是删除操作的话
-                else if(ServiceInstanceOperation.REMOVE.equals(
+                else if (ServiceInstanceOperation.REMOVE.equals(
                         recentlyChangedItem.serviceInstanceOperation)) {
                     Map<String, ServiceInstance> serviceInstanceMap = registry.get(serviceName);
-                    if(serviceInstanceMap != null) {
+                    if (serviceInstanceMap != null) {
                         serviceInstanceMap.remove(serviceInstanceId);
                     }
                 }
@@ -240,6 +186,61 @@ public class CachedServiceRegistry {
                     + changedTimestamp + ", serviceInstanceOperation=" + serviceInstanceOperation + "]";
         }
 
+    }
+
+    /**
+     * 全量拉取注册表的后台线程
+     *
+     * @author huanglk
+     */
+    private class FetchFullRegistryWorker extends Thread {
+        @Override
+        public void run() {
+            // 拉取全量注册表
+            Applications fetchedApplications = httpSender.fetchFullRegistry();
+            while (true) {
+                Applications expectedApplications = applications.getReference();
+                int expectedStamp = applications.getStamp();
+                if (applications.compareAndSet(expectedApplications, fetchedApplications,
+                        expectedStamp, expectedStamp + 1)) {
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 增量拉取注册表的后台线程
+     *
+     * @author huanglk
+     */
+    private class FetchDeltaRegistryWorker extends Thread {
+        @Override
+        public void run() {
+            while (registerClient.isRunning()) {
+                try {
+                    Thread.sleep(SERVICE_REGISTRY_FETCH_INTERVAL);
+
+                    // 拉取回来的是最近3分钟变化的服务实例
+                    DeltaRegistry deltaRegistry = httpSender.fetchDeltaRegistry();
+
+                    // 一类是注册，一类是删除
+                    // 如果是注册的话，就判断一下这个服务实例是否在这个本地缓存的注册表中
+                    // 如果不在的话，就放到本地缓存注册表里去
+                    // 如果是删除的话，就看一下，如果服务实例存在，就给删除了
+
+                    // 我们这里其实是要大量的修改本地缓存的注册表，所以此处需要加锁
+                    mergeDeltaRegistry(deltaRegistry);
+                    // 再检查一下，跟服务端的注册表的服务实例的数量相比，是否是一致的
+                    // 封装一下增量注册表的对象，也就是拉取增量注册表的时候，一方面是返回那个数据
+                    // 另外一方面，是要那个对应的register-server端的服务实例的数量
+                    reconcileRegistry(deltaRegistry);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     /**
