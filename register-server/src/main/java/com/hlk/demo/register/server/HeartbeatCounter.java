cbf1e93509d5aafd1718e04e1437ab9b25621525
黑柳 <463927289@qq.com>
2020-11-03 11:09:22 +0800

    基于LongAdder的分段CAS机制优化心跳计数器的效率

--- register-server/src/main/java/com/hlk/demo/register/server/HeartbeatCounter.java
+++ register-server/src/main/java/com/hlk/demo/register/server/HeartbeatCounter.java
@@ -1,6 +1,7 @@
 package com.hlk.demo.register.server;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 /**
  * 心跳测量计数器
@@ -18,7 +19,8 @@ public class HeartbeatCounter {
     /**
      * 最近一分钟的心跳次数
      */
-    private AtomicLong latestMinuteHeartbeatRate = new AtomicLong(0L);
+    //private AtomicLong latestMinuteHeartbeatRate = new AtomicLong(0L);
+    private LongAdder latestMinuteHeartbeatRate = new LongAdder();
     /**
      * 最近一分钟的时间戳
      */
@@ -49,14 +51,16 @@ public class HeartbeatCounter {
         // 如果你的服务实例很多的话，1万个服务实例，每秒可能都有很多个请求过来更新心跳
         // 如果在这里加了synchronized的话，会影响并发的性能
         // 换成了AtomicLong原子类之后，不加锁，无锁化，CAS操作，保证原子性，还可以多线程并发
-        latestMinuteHeartbeatRate.incrementAndGet();
+        //latestMinuteHeartbeatRate.incrementAndGet();
+        latestMinuteHeartbeatRate.increment();
     }
 
     /**
      * 获取最近一分钟的心跳次数
      */
     public /**synchronized*/ long get() {
-        return latestMinuteHeartbeatRate.get();
+        //return latestMinuteHeartbeatRate.get();
+        return latestMinuteHeartbeatRate.longValue();
     }
 
     private class Daemon extends Thread {
@@ -67,7 +71,8 @@ public class HeartbeatCounter {
                 try {
                     long currentTime = System.currentTimeMillis();
                     if(currentTime - latestMinuteTimestamp > 60 * 1000) {
-                        latestMinuteHeartbeatRate = new AtomicLong(0L);
+                        //latestMinuteHeartbeatRate = new AtomicLong(0L);
+                        latestMinuteHeartbeatRate= new LongAdder();
                         latestMinuteTimestamp = System.currentTimeMillis();
                     }
                     Thread.sleep(1000);
